1290699f21d8acab93e89798e4cce0654eaaae92
Susith Rupasinghe <susith03program@gmail.com>
2022-01-04 00:57:45 +0530

    libs updated

--- src/configs/AppConfig.js
+++ src/configs/AppConfig.js
@@ -7,13 +7,12 @@ export const APP_PREFIX_PATH = '/app';
 export const AUTH_PREFIX_PATH = '/auth';
 
 export const THEME_CONFIG = {
-	navCollapsed: false,
-	sideNavTheme: SIDE_NAV_LIGHT,
-	locale: 'en',
-	navType: NAV_TYPE_SIDE,
-	topNavColor: '#3e82f7',
-	headerNavColor: '',
-	mobileNav: false,
-	currentTheme: 'light',
-	direction: DIR_LTR
-};
+	"navType": "SIDE",
+	"sideNavTheme": "SIDE_NAV_LIGHT",
+	"navCollapsed": false,
+	"topNavColor": "#3e82f7",
+	"headerNavColor": "",
+	"locale": "en",
+	"currentTheme": "light",
+	"direction": "ltr"
+  };
